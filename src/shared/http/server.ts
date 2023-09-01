e16b88475413c88e1abcce7d408dbe66cd06501c
codeasrof <wid_rrodrigues@uolinc.com>
2023-09-01 08:00:04 -0300

    adding swagger

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -4,11 +4,15 @@ import 'express-async-errors'
 import cors from 'cors'
 import { AppError } from '@shared/errors/AppError'
 import { router } from './routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from '../../swagger.json'
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(router)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use((error: Error, request: Request, response: Response) => {
   if (error instanceof AppError) {
